a38964e1ee14ccb75b36d19338dbe0aaca80d502
zahardoc <zahardoc@gmail.com>
2017-06-27 13:45:56 +0300

    Skeleton created

---
+++ src/ZNonce.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zdev\ZNonce;
+
+/**
+ * Main plugin class.
+ *
+ * @package Zdev\ZNonce
+ * @since   1.0.0
+ */
+class ZNonce {
+
+}
---
+++ tests/Unit/ZNonceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Zdev\ZNonce\Tests\Unit;
+
+use Zdev\ZNonce\Tests\TestCase;
+
+/**
+ * Tests for ZNonce class.
+ *
+ * @package Zdev\ZNonce\Tests\Unit
+ *
+ * @since   1.0.0
+ */
+class ZNonceTest extends TestCase {
+	public function test1() {
+		$this->assertSame( true, true );
+	}
+}
